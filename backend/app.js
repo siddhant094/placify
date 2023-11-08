a65c065edcb51a555d48526d160589c4c754e950
Siddhant Pandey <pandeysiddhant21@gmail.com>
2023-11-08 19:17:26 +0530

    cors changess

--- backend/app.js
+++ backend/app.js
@@ -20,11 +20,11 @@ app.get('/', (req, res) => {
 
 // app.use((req, res, next) => {
 //     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 //     res.setHeader(
 //         'Access-Control-Allow-Headers',
 //         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 //     );
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 //     next();
 // });
 
