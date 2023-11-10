5f17f152f7ba490e8ecbb075d7c4854fee0d5bb0
Siddhant Pandey <pandeysiddhant21@gmail.com>
2023-11-10 09:54:27 +0530

    changed signup route

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -11,12 +11,12 @@ router.get('/', usersControllers.getUsers);
 
 router.post(
     '/signup',
-    fileUpload.single('image'),
-    [
-        check('name').not().isEmpty(),
-        check('email').normalizeEmail().isEmail(),
-        check('password').isLength({ min: 6 }),
-    ],
+    // fileUpload.single('image'),
+    // [
+    //     check('name').not().isEmpty(),
+    //     check('email').normalizeEmail().isEmail(),
+    //     check('password').isLength({ min: 6 }),
+    // ],
     usersControllers.signup
 );
 
