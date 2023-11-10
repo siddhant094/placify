417de76dee945f40b8589e4df460b5ba3fb439b6
Siddhant Pandey <pandeysiddhant21@gmail.com>
2023-11-10 13:55:56 +0530

    fileUpload edits

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -8,7 +8,7 @@ router.get('/', usersControllers.getUsers);
 
 router.post(
     '/signup',
-    fileUpload.single('image'),
+    [fileUpload.single('image')],
     // [
     //     check('name').not().isEmpty(),
     //     check('email').normalizeEmail().isEmail(),
