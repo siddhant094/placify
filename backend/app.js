df8e49470bd92e99fe5e5fa583a860e7ec0051e8
Siddhant Pandey <pandeysiddhant21@gmail.com>
2023-12-04 13:14:42 +0530

    added assets

--- backend/app.js
+++ backend/app.js
@@ -43,11 +43,11 @@ app.get('/', (req, res) => {
 });
 
 app.use((error, req, res, next) => {
-    if (req.file) {
-        fs.unlink(req.file.path, (err) => {
-            console.log(err);
-        });
-    }
+    // if (req.file) {
+    //     fs.unlink(req.file.path, (err) => {
+    //         console.log(err);
+    //     });
+    // }
     if (res.headerSent) return next(error);
     res.status(error.code || 500);
     res.json({ message: error.message || 'An unknown error occoured' });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,7 +43,6 @@ const App = () => {
                 <Route path='/places/:placeId'>
                     <UpdatePlace />
                 </Route>
-
                 <Redirect to='/' />
             </Switch>
         );
